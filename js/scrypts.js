b7dba7a6a3eba301eda02092ede5966f23edb1d4
Тимофій <tima.coc13@gmail.com>
2018-02-02 23:05:19 +0200

    nex

---
+++ js/scrypts.js
@@ -0,0 +1,61 @@
+$('.your-class').slick({
+        centerMode: true,
+        centerPadding: '100px 60px 0 60px',
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '100px 60px 0 60px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 1,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+});
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" w3-white", "");
+    }
+    x[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " w3-white";
+}
+$(document).ready(function(){
+    $('.ciskei').click(function () {
+        $('.wrap-menu').toggleClass('activ')
+    })
+});
+$(document).ready(function(){
+    $('.ciskei').click(function () {
+        $('.spa').toggleClass('pros')
+    })
+});
